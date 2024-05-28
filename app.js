a2f812c8dbba1a3b8ed384e81e0021e471ff3cba
Illia Loginov <illia.loginov.g@gmail.com>
2024-05-28 23:07:13 +0300

    Add cors

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import healthcheckRouter from './routes/healthcheck.routes.js';
 import filesRouter from './routes/files.routes.js';
+import cors from 'cors';
+import { corsOptions } from './config/server.config.js';
 
 const app = express();
 
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use('/healthcheck', healthcheckRouter);
--- config/server.config.js
+++ config/server.config.js
@@ -1,3 +1,8 @@
-const { PORT, NODE_ENV = 'development' } = process.env;
+const { PORT, NODE_ENV = 'development', CLIENT_URL } = process.env;
 
 export { PORT as port, NODE_ENV as env };
+
+export const corsOptions = {
+  origin: CLIENT_URL,
+  optionsSuccessStatus: 200
+};
