0bb561f984774f2d79168dd62dccfd79816c4c61
Illia Loginov <illia.loginov.g@gmail.com>
2024-05-29 23:11:49 +0300

    Improve error handling response

--- middleware/errorHandler.middleware.js
+++ middleware/errorHandler.middleware.js
@@ -3,12 +3,12 @@ import { isMulterValidationError } from './multipart.middleware.js';
 
 export default (error, req, res, next) => {
   if (error.status) {
-    res.status(error.status).json(error.message);
+    res.status(error.status).json({ message: error.message });
   } else if (
     error instanceof Joi.ValidationError ||
     isMulterValidationError(error)
   ) {
-    res.status(400).json(error.message);
+    res.status(400).json({ message: error.message });
   } else {
     console.error(error);
 
