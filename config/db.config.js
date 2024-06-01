621eda3974bfbd4997b57d06362fa172ac03756b
Illia Loginov <illia.loginov.g@gmail.com>
2024-06-01 18:28:58 +0300

    Only use SSL for DB connection in prod

--- config/db.config.js
+++ config/db.config.js
@@ -1,12 +1,18 @@
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
+import { env } from './server.config.js';
 
-export default {
+const dbConfig = {
   user: DB_USER,
   password: DB_PASSWORD,
   host: DB_HOST,
   port: DB_PORT,
-  database: DB_NAME,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  database: DB_NAME
 };
+
+if (env === 'production') {
+  dbConfig.ssl = {
+    rejectUnauthorized: false
+  };
+}
+
+export default dbConfig;
